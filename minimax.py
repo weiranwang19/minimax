365e9e1ebdd1adcac3fdd0e9601e601be2cdfde0
weiran <weiranwang@ttic.edu>
2026-04-01 22:59:19 -0500

    basic

--- minimax.py
+++ minimax.py
@@ -10,9 +10,11 @@ class MinimaxGD(Optimizer):
     A custom PyTorch optimizer implementing the Algorithm 5 of
         First-order penalty methods for bilevel optimization.
     https://arxiv.org/pdf/2301.01716
+
+    This method optimizes a strongly-convex-strongly-concave function.
     """
 
-    def __init__(self, params_x, params_y, sigma_x, sigma_y, lip, prox_x, prox_y, lr=1e-3):
+    def __init__(self, params_x, params_y, h_bar, sigma_x, sigma_y, lip, prox_x, prox_y, tol, lr=1e-3):
         # 1. Validate your hyperparameters
         # lr will be the initial step size maintained by learning rate scheduler.
         if lr < 0.0:
@@ -24,11 +26,11 @@ class MinimaxGD(Optimizer):
         # 3. Initialize the base Optimizer class.
         # This will automatically create self.param_groups using the defaults.
         super().__init__([{'params':params_x}, {'params':params_y}], defaults)
-        # Weiran: check what is in self.param_groups.
         # We create two param groups, x and y.
         assert len(self.param_groups) == 2
 
         # Inputs to the algorithm.
+        self.h_bar = h_bar  # function handle.
         self.sigma_x = sigma_x
         self.sigma_y = sigma_y
         self.alpha_bar = min(1, math.sqrt(8 * sigma_y / sigma_x))
@@ -38,66 +40,91 @@ class MinimaxGD(Optimizer):
         self.xi = 1 / (2 * math.sqrt(5) * (1 + 8 * lip / sigma_x))
         self.gamma_x = self.gamma_y = 8 / sigma_x
         self.hat_xi = min(sigma_x, sigma_y) / (lip ** 2)
+        self.tol = tol
 
         self.prox_x = prox_x
         self.prox_y = prox_y
 
         # 4. Initialize the internal state 'z' for all 'x' parameters
+        self.z = []
+        self.zf = []
         # self.param_groups[0] corresponds to params_x
         for p in self.param_groups[0]['params']:
-            import pdb;pdb.set_trace()
-            state = self.state[p]
             # Use .clone().detach() to ensure z is a separate memory block,
             # doesn't track gradients, but shares the same shape/device as p.
-            state['z'] = -p.clone().detach()
-
+            self.z.append(sigma_x * p.clone().detach())
+            self.zf.append(sigma_x * p.clone().detach())
         print("Finished initialization!")
 
-#     @torch.no_grad()
-#     def step(self):
-#         """
-#         Performs a single optimization step. Which corresponds to an outer loop of the algorithm.
-#         """
-#
-#         # Iterate over each parameter group
-#         for group in self.param_groups:
-#             lr = group['lr']
-#
-#             # Iterate over the parameters within this group
-#             for p in group['params']:
-#                 # Skip parameters that have no gradient computed
-#                 if p.grad is None:
-#                     continue
-#
-#                 grad = p.grad
-#
-#                 # ==========================================
-#                 # State Initialization & Tracking
-#                 # ==========================================
-#                 state = self.state[p]
-#
-#                 # If this is the first time we are updating this parameter,
-#                 # initialize its state variables.
-#                 if len(state) == 0:
-#                     state['step'] = 0
-#                     # Example of initializing a state tensor:
-#                     # state['momentum_buffer'] = torch.zeros_like(p, memory_format=torch.preserve_format)
-#
-#                 state['step'] += 1
-#
-#                 # ==========================================
-#                 # The Update Math
-#                 # ==========================================
-#
-#                 # 2. Compute the actual parameter update
-#                 # For SignSGD, we take the sign of the gradient
-#                 update = torch.sign(grad)
-#
-#                 # 3. Apply the update to the parameter
-#                 # p = p - lr * update
-#                 p.sub_(update, alpha=lr)
-#
-#         return
+    def get_x(self):
+        return self.param_groups[0]['params']
 
+    def get_y(self):
+        return self.param_groups[1]['params']
+
+    @torch.no_grad()
+    def assign_x(self, val):
+        for p, v in zip(self.param_groups[0]['params'], val):
+            p.data.copy_(v)
+
+    @torch.no_grad()
+    def assign_y(self, val):
+        for p, v in zip(self.param_groups[1]['params'], val):
+            p.data.copy_(v)
+
+    @torch.no_grad()
+    def scale_vars(self, vars, s):
+        return [s * p for p in vars]
+
+    @torch.no_grad()
+    def add_vars(self, vars1, vars2):
+        return [a + b for a, b in zip(vars1, vars2)]
+
+    #
+    # @torch.no_grad()
+    # def step(self):
+    #     """
+    #     Performs a single outer loop.
+    #     """
+    #
+    #     x = self.param_groups[0]
+    #         lr = group['lr']
+    #
+    #         # Iterate over the parameters within this group
+    #         for p in group['params']:
+    #             # Skip parameters that have no gradient computed
+    #             if p.grad is None:
+    #                 continue
+    #
+    #             grad = p.grad
+    #
+    #             # ==========================================
+    #             # State Initialization & Tracking
+    #             # ==========================================
+    #             state = self.state[p]
+    #
+    #             # If this is the first time we are updating this parameter,
+    #             # initialize its state variables.
+    #             if len(state) == 0:
+    #                 state['step'] = 0
+    #                 # Example of initializing a state tensor:
+    #                 # state['momentum_buffer'] = torch.zeros_like(p, memory_format=torch.preserve_format)
+    #
+    #             state['step'] += 1
+    #
+    #             # ==========================================
+    #             # The Update Math
+    #             # ==========================================
+    #
+    #             # 2. Compute the actual parameter update
+    #             # For SignSGD, we take the sign of the gradient
+    #             update = torch.sign(grad)
+    #
+    #             # 3. Apply the update to the parameter
+    #             # p = p - lr * update
+    #             p.sub_(update, alpha=lr)
+    #
+    #     return
+    #
 
 
--- test_opt.py
+++ test_opt.py
@@ -4,5 +4,19 @@ from minimax import MinimaxGD
 # Setup variables
 x = torch.randn(10, requires_grad=True)
 y = torch.randn(10, requires_grad=True)
+print(x)
+print(y)
 
-opt = MinimaxGD([x],[y], 0.01, 0.01, 1, None, None)
+opt = MinimaxGD([x],[y], None,0.01, 0.01, 1, None, None, 1e-3)
+
+opt.scale_vars(opt.get_x(), 10)
+print(opt.get_x())
+print(opt.get_y())
+
+opt.assign_x(opt.scale_vars(opt.get_x(), 10))
+print(opt.get_x())
+print(opt.get_y())
+
+# prox onto simplex
+# h_bar is function handle which may update values of a model before computing loss.
+# quickly define a model and get parameters() for params_x and params_y.
