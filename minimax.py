95c3d2f14df0d10875732378628f49d065591c0a
weiran <weiranwang@ttic.edu>
2026-04-04 21:04:01 -0500

    cleanup

--- minimax.py
+++ minimax.py
@@ -3,7 +3,20 @@ import math
 import torch
 from torch.optim import Optimizer
 
-class MinimaxGD(Optimizer):
+
+def compute_norm(vals):
+    s = 0
+    for v in vals:
+        s += torch.sum(torch.square(v))
+    return torch.sqrt(s)
+
+
+def compute_prox(vals, prox_func, prox_coeff):
+    # the prox_func shall solve min_x (1/2) |x - x_0|^2 + prox_coeff * fun(x)
+    return [prox_func(p, prox_coeff) for p in vals]
+
+
+class Minimax_SCSC(Optimizer):
     """
     A custom PyTorch optimizer implementing the Algorithm 5 of
         First-order penalty methods for bilevel optimization.
@@ -38,7 +51,7 @@ class MinimaxGD(Optimizer):
         self.zeta = 1 / (2 * math.sqrt(5) * (1 + 8 * lip / sigma_x))
         self.gamma_x = self.gamma_y = (8 / sigma_x)
         self.zeta_hat = min(sigma_x, sigma_y) / (lip ** 2)
-        print(f"alpha_bar={self.alpha_bar}, eta_z={self.eta_z}, eta_y={self.eta_y}, zeta={self.zeta}, gamma_x={self.gamma_x}, gamma_y={self.gamma_y}, zeta_hat={self.zeta_hat}")
+        print(f"alpha_bar={self.alpha_bar}, sigma_x={self.sigma_x}, sigma_y={self.sigma_y}, eta_z={self.eta_z}, eta_y={self.eta_y}, zeta={self.zeta}, gamma_x={self.gamma_x}, gamma_y={self.gamma_y}, zeta_hat={self.zeta_hat}")
 
         self.lr = lr
         self.tol = tol
@@ -94,18 +107,6 @@ class MinimaxGD(Optimizer):
     @torch.no_grad()
     def add_vals(self, vals1, vals2):
         return [a + b for a, b in zip(vals1, vals2)]
-
-    @torch.no_grad()
-    def compute_prox(self, vals, prox_func, prox_coeff):
-        # the prox_func shall solve min_x (1/2) |x - x_0|^2 + prox_coeff * fun(x)
-        return [prox_func(p, prox_coeff) for p in vals]
-
-    @torch.no_grad()
-    def compute_norm(self, vals):
-        s = 0
-        for v in vals:
-            s += torch.sum(torch.square(v))
-        return torch.sqrt(s)
 
     def compute_h_bar_gradient(self, x_val, y_val):
         # We assume h_bar is already a function of x and y, e.g., h_bar forwards a model whose variables are x and y.
@@ -166,8 +167,8 @@ class MinimaxGD(Optimizer):
             a_x_k, a_y_k = self.compute_a_k(x_k_m1, y_k_m1, z_g_k, y_g_k)
             x_tmp = self.add_vals(x_k_m1, self.scale_vals(a_x_k, - self.zeta * self.gamma_x))
             y_tmp = self.add_vals(y_k_m1, self.scale_vals(a_y_k, - self.zeta * self.gamma_y))
-            x_k_0 = x_k_t = self.compute_prox(x_tmp, self.prox_x, self.zeta * self.gamma_x)
-            y_k_0 = y_k_t = self.compute_prox(y_tmp, self.prox_y, self.zeta * self.gamma_y)
+            x_k_0 = x_k_t = compute_prox(x_tmp, self.prox_x, self.zeta * self.gamma_x)
+            y_k_0 = y_k_t = compute_prox(y_tmp, self.prox_y, self.zeta * self.gamma_y)
             b_x_k_t = self.scale_vals(self.add_vals(x_tmp, self.scale_vals(x_k_t, -1)), 1 / (self.zeta * self.gamma_x))
             b_y_k_t = self.scale_vals(self.add_vals(y_tmp, self.scale_vals(y_k_t, -1)), 1 / (self.zeta * self.gamma_y))
 
@@ -182,11 +183,11 @@ class MinimaxGD(Optimizer):
                 a_x_t, a_y_t = self.compute_a_k(x_k_t, y_k_t, z_g_k, y_g_k)
                 x_tmp = self.add_vals(a_x_t, b_x_k_t)
                 y_tmp = self.add_vals(a_y_t, b_y_k_t)
-                lhs = self.gamma_x * self.compute_norm(x_tmp) ** 2 + self.gamma_y * self.compute_norm(y_tmp) ** 2
+                lhs = self.gamma_x * compute_norm(x_tmp) ** 2 + self.gamma_y * compute_norm(y_tmp) ** 2
                 x_diff = self.add_vals(x_k_t, self.scale_vals(x_k_m1, -1))
                 y_diff = self.add_vals(y_k_t, self.scale_vals(y_k_m1, -1))
-                rhs = (1 / self.gamma_x) * self.compute_norm(x_diff) ** 2 + (1 / self.gamma_y) * self.compute_norm(y_diff) ** 2
-                if lhs <= rhs:
+                rhs = (1 / self.gamma_x) * compute_norm(x_diff) ** 2 + (1 / self.gamma_y) * compute_norm(y_diff) ** 2
+                if lhs <= rhs + 1e-8 or t>10000:  # Weiran: added slackness.
                     if debug:
                         print(f"k={k}: inner check passed at t={t}, lhs={lhs}, rhs={rhs}")
                     break
@@ -204,11 +205,11 @@ class MinimaxGD(Optimizer):
                 # line 12
                 x_k_tp1_before_prox = self.add_vals(x_k_t, self.scale_vals(x_diff, beta_t))
                 x_k_tp1_before_prox = self.add_vals(x_k_tp1_before_prox, self.scale_vals(a_x_t_half, - self.zeta * self.gamma_x))
-                x_k_tp1 = self.compute_prox(x_k_tp1_before_prox, self.prox_x, self.zeta * self.gamma_x)
+                x_k_tp1 = compute_prox(x_k_tp1_before_prox, self.prox_x, self.zeta * self.gamma_x)
                 # line 13
                 y_k_tp1_before_prox = self.add_vals(y_k_t, self.scale_vals(y_diff, beta_t))
                 y_k_tp1_before_prox = self.add_vals(y_k_tp1_before_prox, self.scale_vals(a_y_t_half, - self.zeta * self.gamma_y))
-                y_k_tp1 = self.compute_prox(y_k_tp1_before_prox, self.prox_y, self.zeta * self.gamma_y)
+                y_k_tp1 = compute_prox(y_k_tp1_before_prox, self.prox_y, self.zeta * self.gamma_y)
 
                 # line 14
                 b_x_k_tp1 = self.scale_vals(self.add_vals(x_k_tp1_before_prox, self.scale_vals(x_k_tp1, -1)), 1/(self.zeta * self.gamma_x))
@@ -252,10 +253,14 @@ class MinimaxGD(Optimizer):
             x_grad, y_grad = self.compute_h_bar_gradient(x_kp1, y_kp1)
             # line 23
             x_hat_kp1 = self.add_vals(x_kp1, self.scale_vals(x_grad, - self.zeta_hat))
-            x_hat_kp1 = self.compute_prox(x_hat_kp1, self.prox_x, self.zeta_hat)
+            x_hat_kp1 = compute_prox(x_hat_kp1, self.prox_x, self.zeta_hat)
             # line 24
             y_hat_kp1 = self.add_vals(y_kp1, self.scale_vals(y_grad, self.zeta_hat))
-            y_hat_kp1 = self.compute_prox(y_hat_kp1, self.prox_y, self.zeta_hat)
+            y_hat_kp1 = compute_prox(y_hat_kp1, self.prox_y, self.zeta_hat)
+
+            # The best x and y after outer loop k.
+            self.assign_x(x_hat_kp1)
+            self.assign_y(y_hat_kp1)
 
             # Final check
             x_grad_hat, y_grad_hat = self.compute_h_bar_gradient(x_hat_kp1, y_hat_kp1)
@@ -269,19 +274,62 @@ class MinimaxGD(Optimizer):
             delta_y = self.add_vals(self.scale_vals(delta_y, 1 / self.zeta_hat), self.scale_vals(grad_delta_y, -1))
 
             # line 25
-            delta = self.compute_norm(delta_x + delta_y)
+            delta = compute_norm(delta_x + delta_y)
             if debug:
                 print(f"outer loop {k}, x_kp1={x_kp1}, y_kp1={y_kp1}")
-                print(f"k={k}, x_grad={self.compute_norm(x_grad)}, y_grad={self.compute_norm(y_grad)}")
-                print(f"k={k}, final check norm: {delta}")
+                print(f"outer loop {k}, x_hat_kp1={x_hat_kp1}, y_hat_kp1={y_hat_kp1}")
+                print(f"outer loop k={k}, x_grad={compute_norm(x_grad)}, y_grad={compute_norm(y_grad)}")
+                print(f"outer loop k={k}, final check norm: {delta}")
             if delta < self.tol:
-                if debug:
-                    print(f"outer loop {k}, x_hat_kp1={x_hat_kp1}, y_hat_kp1={y_hat_kp1}")
-                self.assign_x(x_hat_kp1)
-                self.assign_y(y_hat_kp1)
                 break
 
             z_k = z_kp1
             y_k = y_kp1
             z_f_k = z_f_kp1
             y_f_k = y_f_kp1
+
+
+"""
+A custom PyTorch optimizer implementing the Algorithm 6 of
+    First-order penalty methods for bilevel optimization.
+https://arxiv.org/pdf/2301.01716
+
+This method optimizes a non-convex-weakly-concave function.
+"""
+def optimize_NCWC(params_x, params_y, h_func, lip_h, D_y, prox_x, prox_y, epsilon, lr=1, max_iter=1000):
+    x_k = [p.clone().detach() for p in params_x]
+    y_0 = [p.clone().detach() for p in params_y]
+    epsilon_0 = epsilon / 2
+
+    for k in range(max_iter):
+
+        def h_k():
+            f = h_func()
+            s_y = 0
+            for p, p0 in zip(params_y, y_0):
+                s_y += torch.sum(torch.square(p - p0))
+            s_x = 0
+            for p, p0 in zip(params_x, x_k):
+                s_x += torch.sum(torch.square(p - p0))
+            f = f - epsilon / (4 * D_y) * s_y + lip_h * s_x
+            # print(f"h_k={f}")
+            return f
+
+        epsilon_k = epsilon_0 / (k + 1)
+        sigma_y = epsilon / (2 * D_y)
+        lip_k = 3 * lip_h + epsilon / (2 * D_y)
+        # print(f"K={k}, epsilon_k={epsilon_k}")
+        # Variables params_x and params_y are updated by solver.rum().
+        solver = Minimax_SCSC(params_x, params_y, h_k, sigma_x=lip_h, sigma_y=sigma_y, lip=lip_k, prox_x=prox_x, prox_y=prox_y, tol=epsilon_k, lr=lr, max_iter=max_iter)
+        solver.run(debug=False)
+        # At the end of run(), params_x and params_y are updated.
+        x_kp1 = [p.clone().detach() for p in params_x]
+        y_kp1 = [p.clone().detach() for p in params_y]
+
+        diff = compute_norm(solver.add_vals(x_kp1, solver.scale_vals(x_k, -1)))
+        print(f"x_kp1={x_kp1}, y_kp1={y_kp1}, diff={diff}")
+        if diff <= epsilon / (4 * lip_h):
+            return
+        # import pdb;pdb.set_trace()
+
+        x_k = x_kp1
--- test_saddle.py
+++ test_saddle.py
@@ -1,5 +1,6 @@
+import math
 import torch
-from minimax import MinimaxGD
+from minimax import Minimax_SCSC, optimize_NCWC
 
 # Setup simple test
 x = torch.rand(10, requires_grad=True)
@@ -9,19 +10,27 @@ print(y)
 
 sigma_x = 0.1
 sigma_y = 0.1
-lip=10
+lip=2
 
 def h_bar():
     loss = torch.sum(x*y) + (sigma_x/2) * torch.sum(x**2) - (sigma_y/2) * torch.sum(y**2)
-    print(f"loss={loss}")
+    # print(f"loss={loss}")
     return loss
 
 def prox_bound(v, coeff):
     del coeff
-    return torch.clamp(v, min=-2.0, max=2.0)
-    # return v
+    return torch.clamp(v, min=-1.0, max=1.0)
 
 
-opt = MinimaxGD([x],[y], h_bar, sigma_x, sigma_y, lip, prox_bound, prox_bound, tol=1e-2)
+# opt = Minimax_SCSC([x],[y], h_bar, sigma_x, sigma_y, lip, prox_bound, prox_bound, tol=1e-2)
+# opt.run(debug=True)
 
-opt.run()
+
+def h_ncwc():
+    loss = torch.sum(x*y) - 1 * torch.sum(x**2) # - (sigma_y/2) * torch.sum(y**2)
+    # print(f"loss={loss}")
+    return loss
+
+D_y = 10  # math.sqrt(40)
+lip_h = 2
+optimize_NCWC([x], [y], h_ncwc, lip_h, D_y, prox_bound, prox_bound, 1e-2)
